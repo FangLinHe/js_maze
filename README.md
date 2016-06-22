8812d8ecc17cae04c674ac64df6dbfbc240fab3f
Fanny He <fanny_he@htc.com>
2016-06-22 14:38:55 +0800

    add kruskal's algorithm

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ I wrote this simple maze game to practice HTML5's canvas and javascript.  The cu
 You can go to [this website](http://fanglinhe.wix.com/fanglinhe#!javascript/nwibq) to play the game.  It redirects to a page on GoogleDrive, it will be avaialbe at least by the end of July, 2016.  Simply use the arrow keys on your keyboard to move the blue ball and try to reach the goal with red flag.  Number of steps you have moved will be shown as ```Moves: 0``` on the page.  The black bricks are the wall which you can not pass through.  After reaching the goal, a smile face will be shown on the blue ball and the message ```You have reached the goal!``` will be shown on page.  You can press ```Click to restart the game``` to restart the game at anytime.
 
 ## Current status:
+* 22-June-2016: I implemented depth-first Randomized Kruskal's algorithm mentioned [here](https://en.wikipedia.org/wiki/Maze_generation_algorithm#Randomized_Kruskal.27s_algorithm) with loops instead of recursion.  Now planning to add web forms to modify the size of the maze and the maze generation method.  Next I would try to modify the data structure to make the code clearer.
 * 21-June-2016: I implemented depth-first search method mentioned [here](https://en.wikipedia.org/wiki/Maze_generation_algorithm#Depth-first_search) with loops instead of recursion.
 * 20-June-2016: So far, the maze's map is fixed.  I would like to implement several algorithms mentioned on [Wiki page](https://en.wikipedia.org/wiki/Maze_generation_algorithm).
 
--- maze_generator.js
+++ maze_generator.js
@@ -105,3 +105,105 @@ function select_neighbor(r, c, traversed, n) {
   }
   return {r: r+trav_order[to_idx][0], c: c+trav_order[to_idx][1], dir: direction[to_idx]};
 }
+
+// --------------------------------------------------
+
+function kruskal(path_num_rows, path_num_cols) {
+  const num_cells = path_num_rows*path_num_cols;
+  var maze_map = [];
+  var grid_num_cols = path_num_cols*2+1;
+  var grid_num_rows = path_num_rows*2+1;
+  // initialze the map:
+  // fill cells of odd indices (e.g. [1,1], [3,5], etc.) with 1 and the rest with 0.
+  // 1's represent paths, and 0's represent walls.
+  for (var r = 0; r < grid_num_rows; r++) {
+    maze_map[r] = [];
+    for (var c = 0; c < grid_num_cols; c++) {
+      maze_map[r][c] = (r%2 == 1) * (c%2 == 1);
+    }
+  }
+
+  // create a list of all walls
+  var walls = [];
+  // each cell is represented by r*path_num_cols+c,
+  // where r is the row index and c is the column index.
+  // horizontal walls
+  for (var r = 0; r < path_num_rows-1; r++) {
+    for (var c = 0; c < path_num_cols; c++) {
+      var cell1_idx = r*path_num_cols+c;
+      var cell2_idx = (r+1)*path_num_cols+c;
+      var wall_pos = [(r+1)*2, c*2+1];
+      walls.push({cell1_idx: cell1_idx, cell2_idx: cell2_idx, wall_pos: wall_pos});
+    }
+  }
+  // vertical walls
+  for (var r = 0; r < path_num_rows; r++) {
+    for (var c = 0; c < path_num_cols-1; c++) {
+      var cell1_idx = r*path_num_cols+c;
+      var cell2_idx = r*path_num_cols+c+1;
+      var wall_pos = [r*2+1, (c+1)*2];
+      walls.push({cell1_idx: cell1_idx, cell2_idx: cell2_idx, wall_pos: wall_pos});
+    }
+  }
+
+  // create a set for each cell
+  var cell_sets = [];
+  var cell_sets_idx = [];
+  for (var r = 0; r < path_num_rows; r++) {
+    for (var c = 0; c < path_num_cols; c++) {
+      var set_obj = new Set();
+      var idx = r*path_num_cols+c;
+      set_obj.add(idx);
+      cell_sets.push(set_obj);
+      cell_sets_idx.push(idx);
+    }
+  }
+
+  // for each wall, in some random order
+  walls = shuffle(walls);
+  for (let w of walls) {
+    var cell1_idx = w.cell1_idx;
+    var cell2_idx = w.cell2_idx;
+    var wall_pos = w.wall_pos;
+    // if the cells divided by this wall belong to distinct sets
+    if (cell_sets_idx[cell1_idx] != cell_sets_idx[cell2_idx]) {
+      // remove the current wall
+      maze_map[wall_pos[0]][wall_pos[1]] = 1;
+      // join the sets of the formerly divided cells.
+      if (cell_sets[cell_sets_idx[cell1_idx]].size < cell_sets[cell_sets_idx[cell2_idx]].size) {
+        var tmp = cell1_idx;
+        cell1_idx = cell2_idx;
+        cell2_idx = tmp;
+      }
+      var del_set_idx = cell_sets_idx[cell2_idx];
+      for (let item of cell_sets[del_set_idx]) {
+        cell_sets[cell_sets_idx[cell1_idx]].add(item);
+        cell_sets_idx[item] = cell_sets_idx[cell1_idx];
+      }
+      cell_sets[del_set_idx].clear();
+      if (cell_sets[cell_sets_idx[cell1_idx]].size == num_cells)
+        break;
+    }
+  }
+
+  return maze_map;
+}
+
+function shuffle(array) {
+  var current_index = array.length, temporary_value, random_index;
+
+  // While there remain elements to shuffle...
+  while (0 !== current_index) {
+
+    // Pick a remaining element...
+    random_index = Math.floor(Math.random() * current_index);
+    current_index--;
+
+    // And swap it with the current element.
+    temporary_value = array[current_index];
+    array[current_index] = array[random_index];
+    array[random_index] = temporary_value;
+  }
+
+  return array;
+}
